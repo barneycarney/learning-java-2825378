6a4be0b3c83c37ce2825e4b94c26ab06eb7aa7af
Barney Carney <Barney.Carney@childrens.harvard.edu>
2020-07-08 23:20:12 -0400

    Added Maven Project. Removed Original hw.java

---
+++ learning-java-2825378/src/main/java/org/chboston/bcarney/maven/App.java
@@ -0,0 +1,13 @@
+package org.chboston.bcarney.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
